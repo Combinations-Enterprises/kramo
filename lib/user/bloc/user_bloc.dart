08b724dc80b89ba52019f2cb7b6fee94eae72475
gallottohugo <gallottohugo@gmail.com>
2020-10-12 20:17:25 -0300

    Add user bloc

---
+++ lib/user/bloc/user_bloc.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:kramo/user/repository/auth_repository.dart';
+
+class UserBloc implements Bloc{
+
+	final _authRepository = AuthRepository();
+
+	Stream<firebase_auth.User> streamFirebase = firebase_auth.FirebaseAuth.instance.authStateChanges();
+	Stream<firebase_auth.User> get authStatus => streamFirebase;
+
+	Future<firebase_auth.User> signIn() {
+		return _authRepository.signInFirebase();
+	}
+
+    @override
+    void dispose() {
+    	
+  	}
+}

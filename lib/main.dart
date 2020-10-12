478d1a7d7b8f96628ece6c58d1a3c8def0aa896c
gallottohugo <gallottohugo@gmail.com>
2020-10-12 20:18:03 -0300

    Remove unuse code

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:kramo/User/ui/screen/login_screen.dart';
 import 'package:kramo/routes/routes.dart';
+import 'package:kramo/user/bloc/user_bloc.dart';
 
-void main() {
-  	runApp(MyApp());
-}
+
+void main() async {
+	WidgetsFlutterBinding.ensureInitialized();
+	await Firebase.initializeApp();
+	runApp(MyApp());
+} 
+
+
 
 class MyApp extends StatelessWidget {
   	// This widget is the root of your application.
-  	@override
-  	Widget build(BuildContext context) {
-    	return MaterialApp(
-			debugShowCheckedModeBanner: false,
-      		title: 'Flutter Demo',
-			routes: ApplicationRoutes.getApplicationRoutes(),
-      		theme: ThemeData(
-				primarySwatch: Colors.blue,
-				visualDensity: VisualDensity.adaptivePlatformDensity,
-				
-      		),
-      		//home: MyHomePage(title: 'Kramo'),
-			home: LoginScreen(),
-    	);
-  	}
-}
 
-class MyHomePage extends StatefulWidget {
-  	MyHomePage({Key key, this.title}) : super(key: key);
-
-  	final String title;
-
-  	@override
-  	_MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  	@override
-  	Widget build(BuildContext context) {
-    	return Scaffold(
-      		appBar: AppBar(title: Text(widget.title),),
-      		body: Container()
-    	);
-  	}
+	@override
+	Widget build(BuildContext context) {
+		return BlocProvider(
+			child: MaterialApp(
+				routes: ApplicationRoutes.getApplicationRoutes(),
+				debugShowCheckedModeBanner: false,
+          		title: 'Flutter Demo',
+          		home: LoginScreen(),
+        	),
+			bloc: UserBloc()
+		);
+	}
 }

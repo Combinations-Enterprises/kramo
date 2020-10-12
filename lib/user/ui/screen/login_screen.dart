dbaf91f31657c7230dd4405013f7b14bf34dda3e
gallottohugo <gallottohugo@gmail.com>
2020-10-12 20:18:45 -0300

    -

--- lib/user/ui/screen/login_screen.dart
+++ lib/user/ui/screen/login_screen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:kramo/main_screen.dart';
+import 'package:kramo/user/bloc/user_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
   	LoginScreen({Key key}) : super(key: key);
@@ -7,10 +11,28 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+	UserBloc userBloc;
+
   	@override
   	Widget build(BuildContext context) {
-    	return _loginGoogleUI();
+		userBloc = BlocProvider.of(context);
+    	return _handleCurrentSession();
   	}
+
+	Widget _handleCurrentSession(){
+		return StreamBuilder(
+			stream: userBloc.authStatus,
+			builder: (BuildContext context, AsyncSnapshot snapshot){
+				if (!snapshot.hasData || snapshot.hasError){
+					return _loginGoogleUI();
+				} else {
+					return MainScreen();
+				}
+			},
+
+		);
+	}
 
 
 	Widget _loginGoogleUI(){
@@ -19,7 +41,9 @@ class _LoginScreenState extends State<LoginScreen> {
 				child: RaisedButton(
 					child: Text('Login with Gmail'),
 					onPressed: (){
-						
+						userBloc.signIn().then(
+							(firebase_auth.User user) => print("El usuario es ${user.displayName}"))
+						;
 					},
 				),
 			),

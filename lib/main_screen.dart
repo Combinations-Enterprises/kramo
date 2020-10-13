c9e56700c5e8e75b97e6e449b0542860fe827155
gallottohugo <gallottohugo@gmail.com>
2020-10-13 16:29:31 -0300

    Fix page

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:kramo/user/bloc/user_bloc.dart';
+import 'package:kramo/user/ui/widgets/button_firebase_signout_widget.dart';
+import 'package:kramo/user/ui/widgets/user_profile_widget.dart';
 
 class MainScreen extends StatefulWidget {
     MainScreen({Key key}) : super(key: key);
@@ -18,75 +20,13 @@ class _MainScreenState extends State<MainScreen> {
 				crossAxisAlignment: CrossAxisAlignment.center,
 				mainAxisAlignment: MainAxisAlignment.center,
 			  	children: [
-			    	StreamBuilder(
-						stream: userBloc.streamFirebase,
-						builder: (BuildContext context, AsyncSnapshot snapshot){
-							if(!snapshot.hasData || snapshot.hasError){ 
-								return CircularProgressIndicator();
-							} else {
-								return _showProfileData(snapshot);
-							}
-						},
-					),
+					Text('You are logged in', style: TextStyle(fontSize: 20.0),),
+					SizedBox(height: 20.0,),
+					UserProfileWidget(),
 					SizedBox(height: 50,),
-					Center(
-						child: Container(
-							padding: EdgeInsets.all(50),
-							child: Center(
-								child: FlatButton(
-									shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-									color: Colors.redAccent,
-									child: Padding(
-										padding: EdgeInsets.all(10),
-										child: Row(
-											mainAxisAlignment: MainAxisAlignment.center,
-											crossAxisAlignment: CrossAxisAlignment.center,
-											children: <Widget>[
-												Icon(Icons.exit_to_app, color: Colors.white),
-												SizedBox(width: 10),
-												Text('Logout', style: TextStyle(color: Colors.white))
-											],
-										)
-									),
-									onPressed: () {
-										userBloc.signut();
-									},	  
-								),
-							)
-						)
-					)
-			  ],
+					ButtonFirebaseSignOutWidget(userBloc: userBloc)
+			  	],
 			),
 		);
-  	}
-
-	Widget _showProfileData( AsyncSnapshot snapshot){
-		return Column(
-			crossAxisAlignment: CrossAxisAlignment.center,
-			children: [
-				Container(
-					height: 90.0,
-					width: 90.0,
-					decoration: BoxDecoration(
-						border: Border.all(
-							color: Colors.white,
-							width: 2,
-							style: BorderStyle.solid
-						),
-						shape: BoxShape.circle,
-						image: DecorationImage(
-							fit: BoxFit.cover,
-							image: NetworkImage(snapshot.data.photoUrl)
-						)
-					)
-				),
-				Text(snapshot.data.displayName, style: TextStyle(color: Colors.black),),
-				Text(snapshot.data.email, style: TextStyle(color: Colors.black),)
-
-			],
-		);
-
-	}
+  	}	
 }
-
-

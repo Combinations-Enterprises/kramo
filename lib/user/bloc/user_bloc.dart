0a26618c1e87708f4aadd61f389de4357acce597
gallottohugo <gallottohugo@gmail.com>
2020-10-14 16:18:33 -0300

    Create user model

--- lib/user/bloc/user_bloc.dart
+++ lib/user/bloc/user_bloc.dart
@@ -2,7 +2,10 @@
 
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
-import 'package:kramo/user/repository/auth_repository.dart';
+import 'package:kramo/user/model/user_model.dart';
+import 'package:kramo/user/repository/cloud_firestore_api.dart';
+import 'package:kramo/user/repository/cloud_firestore_repository.dart';
+import 'package:kramo/user/repository/firebase_auth_repository.dart';
 
 class UserBloc implements Bloc{
 
@@ -10,9 +13,17 @@ class UserBloc implements Bloc{
 
 	Stream<firebase_auth.User> streamAuthStatus = firebase_auth.FirebaseAuth.instance.authStateChanges();
 
+
+	// Use cases
+	// 1. SignIn
 	Future<firebase_auth.User> signIn() { return _authRepository.signInFirebase(); }
 
+	// 2. SignOut
 	void signut(){ _authRepository.signOut(); }
+
+	// 3. Update User
+	final CloudFirestoreRepository _cloudFirestoreRepository = CloudFirestoreRepository();
+	void updateUserData(UserModel user) => _cloudFirestoreRepository.updateUserData(user);
 
     @override
     void dispose() {

4b8b271b73cdc468c1c7555e1d0c7ca430521565
gallottohugo <gallottohugo@gmail.com>
2020-10-25 20:25:13 -0300

    Add createMatch to firestore repository and api

--- lib/user/repository/cloud_firestore_api.dart
+++ lib/user/repository/cloud_firestore_api.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:kramo/match/model/match_model.dart';
 import 'package:kramo/user/model/user_model.dart';
 
 class CloudFirestoreApi {
 	final String collectionUsers = 'users';
 	final String collectionMatches = "matches";
-
 
 	final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
@@ -18,9 +19,22 @@ class CloudFirestoreApi {
 			"matches": user.matches,
 			"updated": DateTime.now()
 		});
-
-
 	}
 
 
+	void createMatch(MatchModel match) async {
+
+
+		Map<String, String> userData = {
+  			'player1': "users/${firebase_auth.FirebaseAuth.instance.currentUser.uid}/"
+		};
+
+
+		DocumentReference reference = await _firestore.collection(collectionMatches).add({
+			"name": "Nueva partida",
+			"player1": _firestore.collection(collectionUsers).doc(firebase_auth.FirebaseAuth.instance.currentUser.uid),
+		});
+
+		
+	}
 }
--- lib/user/repository/cloud_firestore_repository.dart
+++ lib/user/repository/cloud_firestore_repository.dart
@@ -1,8 +1,13 @@
+import 'package:kramo/match/model/match_model.dart';
 import 'package:kramo/user/model/user_model.dart';
 import 'package:kramo/user/repository/cloud_firestore_api.dart';
 
 class CloudFirestoreRepository{
 	final CloudFirestoreApi _cloudFirestoreApi = CloudFirestoreApi();
 
+	// Users 
 	void updateUserData(UserModel user) => _cloudFirestoreApi.updateUserData(user);
+
+	// Matches
+  	void createMatch(MatchModel match) => _cloudFirestoreApi.createMatch(match);
 }
